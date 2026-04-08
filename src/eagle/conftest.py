fc713c38808a660db7fc36f7903f49f6e5daf099
Paul Madden <paul.madden@noaa.gov>
2026-04-08 16:56:49 +0000

    WIP

---
+++ src/eagle/conftest.py
@@ -0,0 +1,81 @@
+import json
+import re
+from collections.abc import Callable
+from copy import deepcopy
+from pathlib import Path
+from typing import Any
+
+from pytest import fixture
+from uwtools.api.config import validate
+
+
+@fixture
+def logged(caplog):
+    def f(s: str):
+        found = any(re.match(rf"^.*{s}.*$", message) for message in caplog.messages)
+        caplog.clear()
+        return found
+
+    return f
+
+
+@fixture
+def validator():
+    def f(testpath: str, name: str, tmp_path: Path, *args: Any) -> Callable:
+        """
+        Returns a lambda that validates an eventual config argument.
+
+        :param testpath: Path to calling test module.
+        :param name: Schema filename stem in driver package directory.
+        :param args: Keys leading to sub-schema to be used to validate config.
+        """
+        schema = json.loads((Path(testpath).parent / f"{name}.jsonschema").read_text())
+        defs = schema.get("$defs", {})
+        for arg in args:
+            schema = schema[arg]
+        if args and args[0] != "$defs":
+            schema.update({"$defs": defs})
+        path = tmp_path / "test.schema"
+        path.write_text(json.dumps(schema))
+        return lambda c: validate(schema_file=path, config_data=c)
+
+    return f
+
+
+@fixture
+def with_del():
+    def f(d: dict, *args: Any) -> dict:
+        """
+        Delete a value at a given chain of keys in a dict.
+
+        :param d: The dict to update.
+        :param args: One or more keys navigating to the value to delete.
+        """
+        new = deepcopy(d)
+        p = new
+        for key in args[:-1]:
+            p = p[key]
+        del p[args[-1]]
+        return new
+
+    return f
+
+
+@fixture
+def with_set():
+    def f(d: dict, val: Any, *args: Any) -> dict:
+        """
+        Set a value at a given chain of keys in a dict.
+
+        :param d: The dict to update.
+        :param val: The value to set.
+        :param args: One or more keys navigating to the value to set.
+        """
+        new = deepcopy(d)
+        p = new
+        for key in args[:-1]:
+            p = p[key]  # pragma: no cover
+        p[args[-1]] = val
+        return new
+
+    return f
---
+++ src/eagle/data/test_data.py
@@ -0,0 +1,52 @@
+CONFIG_GRIDS_AND_MESHES = {
+    "grids_and_meshes": {
+        "filenames": {
+            "gfs_target_grid": "/path/to/global_one_degree.nc",
+            "hrrr_target_grid": "/path/to/hrrr_15km.nc",
+            "latent_mesh": "/path/to/latentx2.spongex1.combined.sorted.npz",
+        },
+        "rundir": "/path/to/rundir",
+    },
+}
+
+
+def test_grids_and_meshes_top(logged, tmp_path, validator, with_del, with_set):
+    ok = validator(__file__, "grids_and_meshes", tmp_path)
+    config = CONFIG_GRIDS_AND_MESHES
+    # Basic correctness:
+    assert ok(config)
+    # Certain top-level keys are required:
+    for key in ["grids_and_meshes"]:
+        assert not ok(with_del(config, key))
+        assert logged(f"'{key}' is a required property")
+    # Additional keys are allowed:
+    assert ok(with_set(config, "foo", "bar"))
+    # Some keys have object values:
+    for key in ["grids_and_meshes"]:
+        assert not ok(with_set(config, None, key))
+        assert logged("is not of type 'object'")
+
+
+def test_grids_and_meshes_grids_and_meshes(
+    logged, tmp_path, validator, with_del, with_set
+):
+    ok = validator(
+        __file__, "grids_and_meshes", tmp_path, "properties", "grids_and_meshes"
+    )
+    config = CONFIG_GRIDS_AND_MESHES["grids_and_meshes"]
+    # Basic correctness:
+    assert ok(config)
+    # Certain top-level keys are required:
+    for key in ["filenames", "rundir"]:
+        assert not ok(with_del(config, key))
+        assert logged(f"'{key}' is a required property")
+    # Additional keys are not allowed:
+    assert not ok(with_set(config, "foo", "bar"))
+    # Some keys have object values:
+    for key in ["filenames"]:
+        assert not ok(with_set(config, None, key))
+        assert logged("is not of type 'object'")
+    # Some keys have string values:
+    for key in ["rundir"]:
+        assert not ok(with_set(config, None, key))
+        assert logged("is not of type 'string'")
